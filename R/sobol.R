d90263428a7b777e2fd010efbe808f3d74c8488f
J. Derek Tucker <jdtuck@sandia.gov>
2025-07-14 13:07:37 -0600

    working adaptive

--- R/sobol.R
+++ R/sobol.R
@@ -30,3 +30,41 @@ for (i in 1:p){
 for (i in 1:p){
   ST[i] <- sum(object$coef[object$vars[,i] != 0]^2)/sum(object$coeff^2)
 }
+
+## adaptive_khaos
+mcmc_use = 1000
+nbasis = object$nbasis[mcmc_use]
+vars = object$vars[mcmc_use,1:nbasis,]
+coef = object$beta[mcmc_use,1:nbasis]
+p <- ncol(vars)
+ST <- rep(NA, p)
+S <- rep(NA, p)
+
+# generate indices
+idx1 <- !is.na(vars)
+idx <- list()
+for (i in 1:p){
+  idx[[i]] <- which(idx1[,i])
+}
+
+dims <- 1:p
+for (i in 1:p){
+  tmp <- which(idx1[,i])
+  tmp2 <- setdiff(dims, i)
+  idxt <- c()
+  for (j in tmp2){
+    idxt <- c(idxt, which(idx1[,j]))
+  }
+  idx[[i]] <- setdiff(idx[[i]], idxt)
+}
+
+# Sobol
+for (i in 1:p){
+  S[i] <- sum(object$coef[idx[[i]]]^2)/sum(object$coeff^2)
+}
+
+# Total Sobol
+for (i in 1:p){
+  ST[i] <- sum(coef[which(vars==i, arr.ind=T)[,1]]^2)/sum(coef^2)
+}
+
